2c48d1c8909ab2fbf4b17051026269ef2736841d
wjsxorjs <wjsxorjs@naver.com>
2024-04-05 08:18:41 +0900

    JAVA 2024.04.05 0.0

---
+++ Ex20240405/src/amBfClass/BallMoving.java
@@ -0,0 +1,77 @@
+package amBfClass;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+public class BallMoving extends JFrame {
+	
+	Canvas can;
+	
+	KeyAdapter keyAdp;
+	
+	int x=100, y=100;
+	
+	
+	public BallMoving() {
+		
+		can = new Canvas() {
+			@Override
+			public void paint(Graphics g) {
+				
+				g.fillOval(x, y, 50, 50);
+			}
+		};
+		
+		this.add(can);
+		this.setBounds(100, 100, 500, 500);
+		this.setVisible(true);
+		
+		
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+
+		
+		keyAdp = new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int code = e.getKeyCode();
+				
+				if(code == e.VK_RIGHT) {
+					x += 5;
+				} else if(code == e.VK_LEFT) {
+					x -= 5;
+				} else if(code == e.VK_UP) {
+					y -= 5;
+				} else if(code == e.VK_DOWN){
+					y += 5;
+				}
+				
+				can.repaint();
+			}
+		};
+		
+		this.addKeyListener(keyAdp);
+		can.addKeyListener(keyAdp);
+		
+		
+		
+	}
+	
+	
+
+	public static void main(String[] args) {
+		new BallMoving();
+
+	}
+
+}
---
+++ Ex20240405/src/amBfClass/ButtonMoving.java
@@ -0,0 +1,89 @@
+package amBfClass;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ButtonMoving extends JFrame {
+	
+	JButton btnL, btnC, btnR;
+	FlowLayout layout = new FlowLayout(FlowLayout.CENTER);
+	JPanel panel = new JPanel(layout);
+	
+
+	public ButtonMoving() {
+		
+		btnL = new JButton("L");
+		btnC = new JButton("C");
+		btnR = new JButton("R");
+		
+		panel.setBackground(Color.LIGHT_GRAY);
+		panel.add(btnL);
+		panel.add(btnC);
+		panel.add(btnR);
+		this.add(panel,BorderLayout.NORTH);
+		
+		this.setBounds(100, 100, 500, 500);
+		this.setVisible(true);
+		
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		btnL.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				layout.setAlignment(FlowLayout.LEFT);
+				panel.revalidate();
+			}
+		});
+		
+		btnC.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				layout.setAlignment(FlowLayout.CENTER);
+				panel.revalidate();
+				
+			}
+		});
+		
+		btnR.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				layout.setAlignment(FlowLayout.RIGHT);
+				panel.revalidate();
+				
+			}
+		});
+			
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		/*
+		  BorderLayout.NORTH에 버튼 3개
+		  L : flowlayout.left
+		  C : flowlayout.center
+		  R : flowlayout.right
+		 */
+		
+		new ButtonMoving();
+		
+	}
+
+
+
+}
